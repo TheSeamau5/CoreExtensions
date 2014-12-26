410dae313072f4205f3246259f1f35cf7312f9d2
Hassan Hayat <hassan.hayat7@gmail.com>
2014-12-26 18:27:13 +0000

    Extensions to Core Elm Libraries

---
+++ CoreExtensions/Array.elm
@@ -0,0 +1,258 @@
+module CoreExtensions.Array where
+
+{-| Extension to the Core Array Library. Includes several useful functions
+either missing or simply not present from the core Array Library. The goal is
+to make arrays as easy to use as lists.
+
+# Basics
+@docs member, indexOf, reverse
+
+# Create arrays
+@docs initialize2, initialize3
+
+# Putting arrays together
+@docs intersperse, concat
+
+# All the maps!
+@docs map2, map3, map4, map5
+
+# Special maps
+@docs concatMap
+
+-}
+
+import Array (..)
+
+
+member : a -> Array a -> Bool
+member element array =
+  let iterate index =
+    let arrayElement = get index array
+    in
+      case arrayElement of
+        Nothing -> False
+        Just x ->
+          if x == element
+          then True
+          else iterate (index + 1)
+
+  in iterate 0
+
+{-| Get the index of an element in an array.
+Returns the first index if the element is duplicated in the array.
+
+    indexOf 2 (fromList [3,4,5,2,1,2]) == Just 3
+
+    indexOf 1 (fromList [2,3,4]) == Nothing
+
+-}
+indexOf : a -> Array a -> Maybe Int
+indexOf element array =
+  let iterate index =
+    let arrayElement = get index array
+    in
+    case arrayElement of
+      Nothing -> Nothing
+      Just x ->
+        if x == element
+          then Just index
+          else iterate (index + 1)
+
+  in iterate 0
+
+{-| Reverse an array
+
+    reverse (fromList [1,2,3]) == fromList [3,2,1]
+-}
+reverse : Array a -> Array a
+reverse array =
+  let maxIterations = length array
+
+      iterate index =
+        let arrayElement = get index array
+        in
+          case arrayElement of
+            Nothing -> empty
+            Just x -> push x (iterate (index + 1))
+
+  in iterate 0
+
+{-| Initialize a 2 dimensional array. `initialize n m f` creates an `n x m`
+array with the element at index `(i,j)` initialized to the result of `(f i j)`
+
+    initialize2 2 3 (,) ==
+      fromList [fromList [(0,0), (0,1), (0,2)],
+                fromList [(1,0), (1,1), (1,2)]]
+-}
+initialize2 : Int -> Int -> (Int -> Int -> a) -> Array (Array a)
+initialize2 x y constructor =
+  initialize y (\xIndex ->
+    initialize x (\yIndex ->
+        constructor xIndex yIndex
+      )
+    )
+
+initialize3 : Int -> Int -> Int -> (Int -> Int -> Int -> a) ->
+              Array (Array (Array a))
+initialize3 x y z constructor =
+  initialize z (\xIndex ->
+    initialize y (\yIndex ->
+      initialize x (\zIndex ->
+          constructor xIndex yIndex zIndex
+        )
+      )
+    )
+
+
+{-| Places the given value between all members of the given array.
+
+    intersperse "on" (fromList ["turtles", "turtles", "turtles"]) ==
+      fromList ["turtles", "on", "turtles", "on", "turtles"]
+-}
+intersperse : a -> Array a -> Array a
+intersperse value array =
+  let isEven x = x % 2 == 0
+      maxIterations = 2 * (length array) - 1
+
+      iterate index =
+        if index < 0 then empty
+        else
+          if isEven index then
+            case get (index // 2) array of
+              Nothing -> empty
+              Just x  -> push x (iterate (index - 1))
+          else
+            push value (iterate (index - 1))
+
+  in iterate (maxIterations - 1)
+
+
+
+{-| Concatenate a bunch of arrays into a single array:
+
+    concat (fromList [(fromList [1,2]), (fromList [3,4])]) ==
+      fromList [1,2,3,4]
+-}
+concat : Array (Array a) -> Array a
+concat arrayOfArrays =
+  let iterate index =
+    let arrayAtIndex = get index arrayOfArrays
+    in
+      case arrayAtIndex of
+        Just xs -> append xs (iterate (index + 1))
+        Nothing -> empty
+  in iterate 0
+
+
+{-| Map a given function onto an array and flatten the result arrays.
+
+    concatMap f array == concat (map f array)
+-}
+concatMap : (a -> Array b) -> Array a -> Array b
+concatMap f array = concat <| map f array
+
+
+{-| Combine two arrays, combining them with the given function. If one
+array is longer, the extra elements are dropped.
+
+    map2 (+) (fromList [1,2,3]) (fromList [1,2,3,4]) ==
+      fromList [2,4,6]
+
+    map2 (,) (fromList [1,2,3]) (fromList ['a', 'b']) ==
+      fromList [(1,'a'), (2, 'b')]
+
+    pairs : Array a -> Array b -> Array (a,b)
+    pairs lefts rights =
+      map2 (,) lefts rights
+
+-}
+map2 : (a -> b -> result) -> Array a -> Array b -> Array result
+map2 f array1 array2 =
+  let maxIterations = min (length array1) (length array2)
+      iterate index =
+        let array1Element = get index array1
+            array2Element = get index array2
+        in
+          case (array1Element, array2Element) of
+
+            (Just a, Just b) ->
+              push (f a b) (iterate (index - 1))
+
+            _ -> empty
+
+  in iterate maxIterations
+
+
+
+
+map3 : (a -> b -> c -> result) -> Array a -> Array b -> Array c -> Array result
+map3 f array1 array2 array3 =
+  let maxIterations = min (length array1) (min (length array2) (length array3))
+
+      iterate index =
+        let array1Element = get index array1
+            array2Element = get index array2
+            array3Element = get index array3
+        in
+          case (array1Element, array2Element, array3Element) of
+
+            (Just a, Just b, Just c) ->
+              push (f a b c) (iterate (index - 1))
+
+            _ -> empty
+
+  in iterate maxIterations
+
+
+
+
+map4 : (a -> b -> c -> d -> result) ->
+       Array a -> Array b -> Array c -> Array d -> Array result
+map4 f array1 array2 array3 array4 =
+  let maxIterations = min (length array1) <|
+                      min (length array2) <|
+                      min (length array3) (length array4)
+
+      iterate index =
+        let array1Element = get index array1
+            array2Element = get index array2
+            array3Element = get index array3
+            array4Element = get index array4
+        in
+          case (array1Element, array2Element, array3Element, array4Element) of
+
+            (Just a, Just b, Just c, Just d) ->
+              push (f a b c d) (iterate (index - 1))
+
+            _ -> empty
+
+  in iterate maxIterations
+
+
+map5 : (a -> b -> c -> d -> e -> result) ->
+       Array a -> Array b -> Array c -> Array d -> Array e -> Array result
+map5 f array1 array2 array3 array4 array5 =
+  let maxIterations = min (length array1) <|
+                      min (length array2) <|
+                      min (length array3) <|
+                      min (length array4) (length array5)
+
+      iterate index =
+        let array1Element = get index array1
+            array2Element = get index array2
+            array3Element = get index array3
+            array4Element = get index array4
+            array5Element = get index array5
+        in
+          case (array1Element,
+                array2Element,
+                array3Element,
+                array4Element,
+                array5Element) of
+
+              (Just a, Just b, Just c, Just d, Just e) ->
+                push (f a b c d e) (iterate (index - 1))
+
+              _ -> empty
+
+  in iterate (maxIterations - 1)
